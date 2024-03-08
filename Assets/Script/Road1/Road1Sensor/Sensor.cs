22545c0a470f34cf848f588256890ecd1150dc1d
Zikriaa <89811825+Zikriaa@users.noreply.github.com>
2024-03-09 01:38:30 +0500

    All Files

---
+++ Assets/Script/Road1/Road1Sensor/Sensor.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    [SerializeField]CarAILane1 carAI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carAI = GetComponentInParent<CarAILane1>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("trigred");
+            carAI.move = false;
+        }
+        if (other.CompareTag("Signal"))
+        {
+            Road1TrafficLight _road1TrafficLight = other.GetComponentInParent<Road1TrafficLight>();
+            if (_road1TrafficLight != null)
+            {
+                if (_road1TrafficLight.onGreen)
+                {
+                    if (_road1TrafficLight.timerGreen < 5 || _road1TrafficLight.timerGreen <=0)
+                    {
+                        carAI.move = false;
+                    }
+                    else
+                    {
+                        carAI.move = true;
+                    }
+                }
+                else if (_road1TrafficLight.onYellow)
+                {
+                    carAI.move = false;
+                }
+                else if (_road1TrafficLight.onRed)
+                {
+                    carAI.move = false;
+                }
+            }
+            Debug.Log("SignalCheck");
+
+        }
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Signal"))
+        {
+            Road1TrafficLight _road1TrafficLight = other.GetComponentInParent<Road1TrafficLight>();
+            if (_road1TrafficLight != null)
+            {
+                if (_road1TrafficLight.onGreen)
+                {
+                    Invoke("wait", 2f);
+
+                }
+                else if (_road1TrafficLight.onYellow)
+                {
+                    carAI.move = false;
+                }
+                else if (_road1TrafficLight.onRed)
+                {
+                    carAI.move = false;
+
+                }
+            }
+            Debug.Log("SignalCheck");
+        }
+    }
+    void wait()
+    {
+        carAI.move = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        carAI.move = true;   
+    }
+
+}
